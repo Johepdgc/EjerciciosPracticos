a9a79359968a7579f2ca13d659fbab0d9389c352
Johepdgc <88364916+Johepdgc@users.noreply.github.com>
2021-08-02 23:43:58 -0600

    Add files via upload

---
+++ Ejercicio1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//Ejercicio practico saludo y nombre
+namespace EjercicioPractico1
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hola! Es un placer conocerte! {0}",name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjercicioPractico1
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+            Int32 a;
+            Int32 b;
+
+            Console.WriteLine("Escribe a");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Int32 arit = (a + b) * (a - b);
+            Console.WriteLine(arit);
+            Console.ReadKey();
+
+        }
+    }
+}
